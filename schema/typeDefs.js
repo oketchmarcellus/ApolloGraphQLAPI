9572f62e88c42637f760b464279fc3d8dd8b0db7
Your Name <you@example.com>
2025-07-07 13:41:51 +0300

    push2

--- schema/typeDefs.js
+++ schema/typeDefs.js
@@ -4,11 +4,82 @@ const typeDefs = `#graphql
     name: String!
     username: String!
     age: Int!
-    nationality: String!
+    nationality: Nationality!
+    friends: [User]
   }
   
   type Query {
     users: [User!]!
+  }
+  enum Nationality {
+    KENYA
+    NIGERIA
+    GHANA
+    ETHIOPIA
+    TANZANIA
+    UGANDA
+    ZAMBIA
+    ZIMBABWE
+    RWANDA
+    MALAWI
+    CONGO
+    MOZAMBIQUE
+    ANGOLA
+    CAMEROON
+    SENEGAL
+    IVORY_COAST
+    TUNISIA
+    ALGERIA
+    EGYPT
+    MOROCCO
+    LIBYA
+    SOUTH_SUDAN
+    SOMALIA
+    SWITZERLAND
+    BELGIUM
+    AUSTRIA
+    DENMARK
+    FINLAND
+    IRELAND
+    PORTUGAL
+    GREECE
+    POLAND
+    CZECH_REPUBLIC
+    HUNGARY
+    SLOVAKIA
+    BULGARIA
+    ROMANIA
+    NORDIC_COUNTRIES
+    NORDIC_COUNTRIES_EXCEPT_FINLAND
+    CROATIA
+    SLOVENIA
+    LITHUANIA
+    SINGAPORE
+    LATVIA
+    ESTONIA
+    ICELAND
+    LUXEMBOURG
+    NORWAY
+    CANADA
+    USA
+    MEXICO
+    UK
+    FRANCE
+    GERMANY
+    ITALY
+    SPAIN
+    JAPAN
+    CHINA
+    INDIA
+    BRAZIL
+    AUSTRALIA
+    RUSSIA
+    SOUTH_AFRICA
+    ARGENTINA
+    SOUTH_KOREA
+    NETHERLANDS
+    SWEDEN
+    TURKEY
   }
 `;
 
